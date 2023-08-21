9830ef58e63f782be4cb5bbc23d7fb9f56756d8e
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-08-21 12:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+// Quiz questions
+const questions = [
+  {
+    question: "What is the capital of France?",
+    choices: ["Paris", "London", "Berlin", "Madrid"],
+    answer: "Paris",
+  },
+  {
+    question: "What is the highest mountain in the world?",
+    choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
+    answer: "Everest",
+  },
+  {
+    question: "What is the largest country by area?",
+    choices: ["Russia", "China", "Canada", "United States"],
+    answer: "Russia",
+  },
+  {
+    question: "Which is the largest planet in our solar system?",
+    choices: ["Earth", "Jupiter", "Mars"],
+    answer: "Jupiter",
+  },
+  {
+    question: "What is the capital of Canada?",
+    choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
+    answer: "Ottawa",
+  },
+];
 
-let userAnswers = JSON.parse(sessionStorage.getItem("progress")) || [];
+// Retrieve user progress from session storage
+let userAnswers = sessionStorage.getItem("progress");
+if (userAnswers) {
+  userAnswers = JSON.parse(userAnswers);
+} else {
+  userAnswers = [];
+}
 
+// Display the quiz questions and choices
 const questionsElement = document.getElementById("questions");
 
 function renderQuestions() {
@@ -18,11 +53,6 @@ function renderQuestions() {
       if (userAnswers[i] === choice) {
         choiceElement.setAttribute("checked", true);
       }
-      choiceElement.addEventListener("change", (event) => {
-        const selectedChoice = event.target.value;
-        userAnswers[i] = selectedChoice;
-        sessionStorage.setItem("progress", JSON.stringify(userAnswers));
-      });
       const choiceText = document.createTextNode(choice);
       questionElement.appendChild(choiceElement);
       questionElement.appendChild(choiceText);
@@ -32,16 +62,26 @@ function renderQuestions() {
 }
 renderQuestions();
 
+// Save user progress to session storage when an option is selected
+questionsElement.addEventListener("change", function (event) {
+  const selectedChoice = event.target.value;
+  const questionIndex = event.target.getAttribute("name").split("-")[1];
+  userAnswers[questionIndex] = selectedChoice;
+  sessionStorage.setItem("progress", JSON.stringify(userAnswers));
+});
+
+// Submit the quiz and calculate the score
 const submitButton = document.getElementById("submit");
 const scoreElement = document.getElementById("score");
 
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener("click", function () {
+  const correctAnswers = questions.map((question) => question.answer);
   let score = 0;
-  for (let i = 0; i < questions.length; i++) {
-    if (userAnswers[i] === questions[i].answer) {
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (userAnswers[i] === correctAnswers[i]) {
       score++;
     }
   }
-  localStorage.setItem("score", score);
   scoreElement.textContent = `Your score is ${score} out of ${questions.length}.`;
+  localStorage.setItem("score", score);
 });
